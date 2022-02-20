694f85fcd2ff5f45c5e596dcbce15e585edd01bf
Fredohm <fredwaucquez@hotmail.fr>
2022-02-20 09:16:36 +0100

    small refactoring

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # Mail Merge Project
+PLACEHOLDER = "[name]"
 # for each name in invited_names.txt
-with open("Input/Names/invited_names.txt") as names:
-    names_list = names.readlines()
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
 
-with open("Input/Letters/starting_letter.txt") as letter:
+with open("./Input/Letters/starting_letter.txt") as letter_file:
     # replace the [name] placeholder with the actual name.
-    new_letter = letter.read()
-    for name in names_list:
+    letter = letter_file.read()
+    for name in names:
         stripped_name = name.strip("\n")
-        personalized_letter = new_letter.replace("name", stripped_name)
+        personalized_letter = letter.replace(PLACEHOLDER, stripped_name)
         # save the letters in the folder "ReadyToSend".
-        with open(f"Output/ReadyToSend/{stripped_name}_invitation.txt", mode="w") as invitation:
+        with open(f"./Output/ReadyToSend/{stripped_name}_invitation.txt", mode="w") as invitation:
             invitation.write(personalized_letter)
